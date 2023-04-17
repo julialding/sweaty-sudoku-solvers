91c413c447a31bf44d85f975838b417ca17375fe
Christine Tian <christinetian01@gmail.com>
2023-04-17 11:18:16 -0500

    Added printGrid and inGrid

---
+++ grid.h
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class grid {
+
+    //Displays the values in the smaller 3x3 sections of the total 9x9 grid
+	void printGrid(int smallCell[3][3]) {
+		for (int i = 0; i < 3; i++) {
+			for (int k = 0; k < 3; k++) {
+				cout << smallCell[i][k] << endl;
+			}
+		}
+	}
+
+	// checks to see if the value is already in the provided cell
+	bool inGrid(int smallCell[3][3], int value) {
+		for (int i = 0; i < 3; i++) {
+			for (int k = 0; k < 3; k++) {
+				if (smallCell[i][k] == value)
+					return true;
+			}
+		}
+		return false;
+	}
+
+	bool checkCol(int cell2[3][3], int cell3[3][3], int value, int col) {
+		for (int i = 0; i < 3; i++) {
+			for (int k = 0; k < 3; k++) {
+				if (cell2[i][k] == value)
+					return true;
+			}
+		}
+	}
+};
